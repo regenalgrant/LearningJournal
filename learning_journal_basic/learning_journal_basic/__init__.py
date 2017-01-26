249c83d92323c2456d00245a86ef3a2670787b09
regenalgrant <regenal@mac.com>
2017-01-25 17:26:16 -0800

    added files to set up database

--- learning_journal_basic/learning_journal_basic/__init__.py
+++ learning_journal_basic/learning_journal_basic/__init__.py
@@ -6,7 +6,8 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    config.include('.route')
+    config.include('.routes')
+    config.include('.models')
     config.add_static_view(name='static', path='learning_journal_basic:static')
     config.scan()
     return config.make_wsgi_app()
--- learning_journal_basic/learning_journal_basic/models/meta.py
+++ learning_journal_basic/learning_journal_basic/models/meta.py
@@ -1,8 +1,16 @@
-# class MyModel(Base):
-#     __tablename__ = 'models'
-#     id = Column(Integer, primary_key=True)
-#     name = Column(Text)
-#     value = Column(Integer)
-#
-#
-# Index('my_index', MyModel.name, unique=True, mysql_length=255)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.schema import MetaData
+
+# Recommended naming convention used by Alembic, as various different database
+# providers will autogenerate vastly different names making migrations more
+# difficult. See: http://alembic.zzzcomputing.com/en/latest/naming.html
+NAMING_CONVENTION = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=NAMING_CONVENTION)
+Base = declarative_base(metadata=metadata)
